4e42ce60eb081f598685a1f0fc365bb9e30bcac8
Jarod <jarod.streckeisen@heig-vd.ch>
2022-04-26 11:25:01 +0200

    Ajout entête

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*-----------------------------------------------------------------------------------
+Nom de fichier : main.cpp
+Auteur : G. Courbat, J. Streckeisen, A. Martins
+Date de création : 26.04.2022
+Description :
+Remarque :
+Compilateur : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------*/
 #include <stdio.h>
 
 int main() {
