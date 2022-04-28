e511508a16cf9e14b02f83f7d8524d398b4b306a
Jarod <jarod.streckeisen@heig-vd.ch>
2022-04-28 13:23:54 +0200

    Ajout test entré utilisateur

--- main.c
+++ main.c
@@ -7,8 +7,44 @@ Remarque :
 Compilateur : Mingw-w64 g++ 11.2.0
 -----------------------------------------------------------------------------------*/
 #include <stdio.h>
+#include <stdbool.h>
+const unsigned int MIN_NB_BILLE = 1000;
+const unsigned int MAX_NB_BILLE = 30000;
+const unsigned int MIN_NB_ETAGE = 10;
+const unsigned int MAX_NB_ETAGE = 20;
+
+bool testerSaisieNumerique(unsigned int valeur,unsigned int min,unsigned int max);
 
 int main() {
-   printf("Hello, World!\n");
+   unsigned int nombreBille = 0;
+   unsigned int nombreEtage = 0;
+
+   do {
+      printf("Entrez le nombre de billes [1000 - 30000] : ");
+      scanf("%u%[0-9]",&nombreBille);
+      if(!testerSaisieNumerique(nombreBille,MIN_NB_BILLE,MAX_NB_BILLE)){
+         printf("Saisie incorrecte. Veuillez SVP recommencer.\n");
+         fflush(stdin);
+      }
+   }while(!testerSaisieNumerique(nombreBille,MIN_NB_BILLE,MAX_NB_BILLE));
+
+   do{
+      printf("Entrez le nombre de rangees de compteurs [10-20] : ");
+      scanf("%u%[0-9]",&nombreEtage);
+      if(!testerSaisieNumerique(nombreEtage,MIN_NB_ETAGE,MAX_NB_ETAGE)){
+         printf("Saisie incorrecte. Veuillez SVP recommencer.\n");
+         fflush(stdin);
+      }
+   }while(!testerSaisieNumerique(nombreEtage,MIN_NB_ETAGE,MAX_NB_ETAGE));
+
+
    return 0;
+}
+
+bool testerSaisieNumerique(const unsigned int valeur,const unsigned int min,const
+unsigned int max){
+   if(valeur > max || valeur < min){
+      return false;
+   }
+   return true;
 }
